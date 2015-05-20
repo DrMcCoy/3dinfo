5117e54a68f0a9b3bd9eb0e7973394d852770518
Sven Hesse <drmccoy@drmccoy.de>
2015-05-20 20:59:41 +0200

    3DINFO: Update GPLv3 notices to the new recommended format

--- src/capability.h
+++ src/capability.h
@@ -1,22 +1,21 @@
 /* 3DInfo - Print information about your graphics hardware.
  *
- * 3DInfo is the legal property of its developers, whose labels can be
- * found in the AUTHORS file distributed with this source
+ * 3DInfo is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
  * distribution.
  *
- * This program is free software; you can redistribute it and/or
+ * 3DInfo is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 3
  * of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * 3DInfo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * along with 3DInfo. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /** @file capability.h
